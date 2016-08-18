7d80bc1ad44466ccb6701441eaf0e3e20a6cd37d
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2016-08-18 10:06:16 +0200

    improved performance of cumulation of areas

--- tests/test_tweaker.py
+++ tests/test_tweaker.py
@@ -3,7 +3,7 @@ from FileHandler import FileHandler
 import os
 
 
-def test_area_cumulation():
+def test_area_acumulation():
     test_file = os.path.join(os.path.dirname(__file__), 'test_object.stl')
     mesh = FileHandler.loadMesh(test_file)
 
@@ -16,16 +16,15 @@ def test_area_cumulation():
     # test area cumulation
     top_n_areas = tweaker.area_cumulation(content=content, n=default_norm)
 
+    print(top_n_areas)
+
     # check output of tweaker
-    expected_areas = [
-        [[-1.0, 0.0, 0.0], 3596.7468],
-        [[1.0, 0.0, 0.0], 3398.5106],
-        [[0.0, -0.642787, -0.766045], 63.5133],
-        [[0.0, -0.642788, -0.766044], 160.4646],
-        [[0.0, 0.642787, 0.766045], 460.4009],
-        [[0.0, -0.644206, -0.764852], 49.0066],
-        [[0.0, 0.766044, -0.642788], 56.2261]
-    ]
+    expected_areas = [[[1.0, 0.0, 0.0], 3398.5106],
+                      [[-1.0, 0.0, 0.0], 3596.7468],
+                      [[0.0, -0.642787, -0.766045], 63.5133],
+                      [[0.0, -0.642788, -0.766044], 160.4646],
+                      [[-0.0, 0.642787, 0.766045], 460.4009],
+                      [[0.0, 0.766044, -0.642788], 56.2261]]
     assert len(top_n_areas) == len(expected_areas)
     for expected_area in expected_areas:
         # TODO: improve comparison of floats
